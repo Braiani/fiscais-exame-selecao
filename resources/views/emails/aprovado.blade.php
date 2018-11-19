93ed9321816ef72623528ad60cd6486681cd3b6c
Felipe Braiani <felipe.braianitech@gmail.com>
2018-11-19 16:05:09 -0300

    Fixed date on e-mail

--- resources/views/emails/aprovado.blade.php
+++ resources/views/emails/aprovado.blade.php
@@ -28,7 +28,7 @@
 <p>Você foi selecionado para atuar como colaborador, na função de {{ $func }}, no Exame de Seleção {{ $inscricao->exame->ano }} - IFMS <em>Campus</em> Campo Grande.</p>
 <p>
 	A Comissão Local do Exame de Seleção {{ $inscricao->exame->ano }} - IFMS <em>Campus</em> Campo Grande <strong>CONVOCA</strong> V. Sa. para o treinamento de 
-	<u>COLABORADORES</u> que será realizado no <strong>SÁBADO</strong>, dia<strong> <u>25/11/2018, às 09h00</u></strong>, no 
+	<u>COLABORADORES</u> que será realizado no <strong>SÁBADO</strong>, dia<strong> <u>24/11/2018, às 09h00</u></strong>, no 
 	<strong>ANFITEATRO DO BLOCO MULTIUSO DA UNIVERSIDADE FEDERAL DO MATO GROSSO DO SUL - UFMS</strong>, situada na Cidade Universitária, Av. Costa e Silva - Pioneiros.
 </p>
 <p>Ressaltamos que a ausência no treinamento implicará na <strong>EXCLUSÃO</strong> do colaborador na aplicação do exame.</p>
